3cbd358b5fe2b1f4624694c5edbe1312bcb87a3f
Michael Nieman <michael.nieman@libertymutual.com>
2018-06-01 15:22:44 -0400

    Log array of unique cities

--- src/Country/CountryList.js
+++ src/Country/CountryList.js
@@ -3,12 +3,29 @@ import Country from './Country';
 
 const CountryList = props => {
   if (props.data.networks) {
-    return props.data.networks.map(network => {
-      return <Country data={network.location.country} />;
+    ////////////////////
+    let countries = [];
+    props.data.networks.map(network => {
+      // if network.location.country is unique
+      if (!countries.find(element => element == network.location.country)) {
+        countries.push(network.location.country);
+      } else {
+        null;
+      }
+      // else do nothing
     });
+    console.log(countries);
+    ////////////////////
+
+    ////////////////////
+    // return props.data.networks.map(network => {
+    //   return <Country data={network.location.country} />;
+    // });
+    ////////////////////
   } else {
     return null;
   }
+  return null;
 };
 
 export default CountryList;
