7934fbd41adb13d15deeb80f189fcadf36369403
Michael Nieman <michael.nieman@libertymutual.com>
2018-06-15 11:02:15 -0400

    Add City route and link

--- src/City/CityList.js
+++ src/City/CityList.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import City from './City';
 
-const CityList = props =>
+const CityList = props => (
+  console.log(props),
   props.data.map(item => {
     if (item.location.country === props.country) {
       return <City data={item} key={item.id} />;
     }
-  });
+  })
+);
 
 export default CityList;
