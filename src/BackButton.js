684cdcd91fccf1eeb1b5789c060dec058641d0e2
Michael Nieman <michael.nieman@libertymutual.com>
2018-06-25 11:16:25 -0400

    Implement back button on modal

---
+++ src/BackButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const BackButton = props => (
+  <button className="back-button" onClick={props.back}>
+    Back
+  </button>
+);
+
+export default BackButton;
--- src/Shop/ShopContainer.js
+++ src/Shop/ShopContainer.js
@@ -1,41 +1,53 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
 import ShopList from './ShopList';
+import BackButton from '../BackButton';
 
 class ShopContainer extends React.Component {
   constructor(props) {
     super(props);
-    this.handleClick = this.handleClick.bind(this);
+    // this.handleClick = this.handleClick.bind(this);
+    // this.handleOutsideClick = this.handleOutsideClick.bind(this);
     this.state = { isOpen: false };
   }
 
-  handleClick() {
-    if (!this.state.isOpen) {
-      document.addEventListener('click', this.handleClick, false);
-    } else {
-      document.removeEventListener('click', this.handleClick, false);
-    }
-    this.setState(prevState => ({
-      isOpen: !prevState.isOpen
-    }));
-  }
+  // handleClick() {
+  //   if (!this.state.isOpen) {
+  //     document.addEventListener('click', this.handleClick, false);
+  //   } else {
+  //     document.removeEventListener('click', this.handleClick, false);
+  //   }
+  //   this.setState(prevState => ({
+  //     isOpen: !prevState.isOpen
+  //   }));
+  // }
+
+  // handleOutsideClick(e) {
+  //   if (this.node.contains(e.target)) {
+  //     return;
+  //   }
+  //   this.handleClick();
+  // }
 
   render() {
     console.log('Container props:', this.props);
-    console.log('Container state:', this.state);
+    // console.log('Container state:', this.state);
 
     if (this.props.data) {
       const city = this.props.match.params.city;
       return (
         <div
+          className="modal-container"
           className="backdrop"
-          ref={node => {
-            this.node = node;
-          }}
+          // ref={node => {
+          //   this.node = node;
+          // }}
         >
-          <div className="modal">
-            <h4>Shops in {city}:</h4>
-            <ShopList data={this.props.data} city={city} />
+          <div>
+            <div className="modal">
+              <h4>Shops in {city}:</h4>
+              <ShopList data={this.props.data} city={city} />
+              <BackButton back={this.props.history.goBack} />
+            </div>
           </div>
         </div>
       );
@@ -43,12 +55,6 @@ class ShopContainer extends React.Component {
       return null;
     }
   }
-
-  // PropTypes = {
-  //   onClose: PropTypes.func.isRequired,
-  //   show: PropTypes.bool,
-  //   children: PropTypes.node
-  // };
 }
 
 export default ShopContainer;
