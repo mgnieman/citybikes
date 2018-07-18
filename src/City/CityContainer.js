186c403fc5e4d7918fd0009a17a470935bd60577
Michael Nieman <michael.nieman@libertymutual.com>
2018-07-18 10:47:58 -0400

    Place blank map next to list of cities

--- src/City/CityContainer.js
+++ src/City/CityContainer.js
@@ -5,15 +5,20 @@ import DataUtil from '../DataUtil';
 
 const CityContainer = props => {
   const country = props.match.params.country;
-  const mapInfo = DataUtil.calculateCenter(country, {}, props.data);
+  const mapInfo = DataUtil.calculateCenter(country, props.data);
+  console.log(mapInfo);
   return (
     <div className="city-container">
-      <h4>Select a city in {country}</h4>
-      <ul>
-        <CityList data={props.data} country={country} />
-        <br />
+      <div className="city-list">
+        <h4>Select a city in {country}</h4>
+        <ul>
+          <CityList data={props.data} country={country} />
+          <br />
+        </ul>
+      </div>
+      <div className="do-i-need-this?">
         <MyMapComponent isMarkerShown mapInfo={mapInfo} />
-      </ul>
+      </div>
     </div>
   );
 };
